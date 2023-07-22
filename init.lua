cff31b9bc6a0c35e7498bda9becdeca49179f9b8
Manu Tamminen <mavata@gmail.com>
2023-07-22 12:53:47 +0300

    Added LSP configs

--- init.lua
+++ init.lua
@@ -1,45 +1,7 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-
-Kickstart.nvim is *not* a distribution.
-
-Kickstart.nvim is a template for your own configuration.
-  The goal is that you can read every line of code, top-to-bottom, understand
-  what your configuration is doing, and modify it to suit your needs.
-
-  Once you've done that, you should start exploring, configuring and tinkering to
-  explore Neovim!
-
-  If you don't know anything about Lua, I recommend taking some time to read through
-  a guide. One possible example:
-  - https://learnxinyminutes.com/docs/lua/
-
-  And then you can explore or search through `:help lua-guide`
-
-
-Kickstart Guide:
-
-I have left several `:help X` comments throughout the init.lua
-You should run that command and read that help section for more information.
-
-In addition, I have some `NOTE:` items throughout the file.
-These are for you, the reader to help understand what is happening. Feel free to delete
-them once you know what you're doing, but they should serve as a guide for when you
-are first encountering a few different constructs in your nvim config.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now :)
---]]
--- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.maplocalleader = 'ä'
 
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
@@ -204,6 +166,9 @@ require('lazy').setup({
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
   -- { import = 'custom.plugins' },
+  {
+  'jalvesaq/Nvim-R'
+  },
 }, {})
 
 -- [[ Setting options ]]
@@ -306,7 +271,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'julia' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
@@ -425,6 +390,9 @@ end
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
+  julials = {},
+  r_language_server = {},
+  pyright = {},
   -- clangd = {},
   -- gopls = {},
   -- pyright = {},
@@ -510,6 +478,62 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
+
+-- Additional keybindings
+
+vim.keymap.set({'n', 'v'}, 'ö', '$')
+vim.keymap.set('i', 'kj', '<ESC>')
+
+
+-- R keybindings
+
+vim.api.nvim_create_autocmd({"FileType", "BufEnter"},
+  { pattern = {"*.R", "*.r"},
+    callback = function()
+      vim.keymap.set('n', 'tt', ':call StartR("R")<CR><C-w>H')
+      vim.keymap.set('n', '<C-c><C-c>', ':call SendParagraphToR("echo", "down")<CR>')
+      vim.keymap.set('i', '<C-c><C-c>', '<Esc>:call SendParagraphToR("echo", "down")<CR>')
+      vim.keymap.set({'n', 'i'}, 'åå', '<Esc>:normal! $a %>%<CR>')
+      vim.keymap.set({'n', 'i'}, 'ÅÅ', '<Esc>:s/ %>%//<CR>')
+      vim.keymap.set({'n', 'i'}, '--', '<Esc>:normal! a <-<CR>a')
+  end
+  })
+
+
+-- Additional setup
+
+vim.o.relativenumber = true
+
+
+-- Run Julia LSP
+require'lspconfig'.julials.setup{
+    on_new_config = function(new_config, _)
+        local julia = vim.fn.expand("~/.julia/environments/nvim-lspconfig/bin/julia")
+        if require'lspconfig'.util.path.is_file(julia) then
+	    -- vim.notify("Hello!")
+            new_config.cmd[1] = julia
+        end
+    end
+}
+
+-- R (r_language_server) ======================================================
+local on_attach_custom = function(client, bufnr)
+  local function buf_set_option(name, value)
+    vim.api.nvim_buf_set_option(bufnr, name, value)
+  end
+  buf_set_option('omnifunc', 'v:lua.MiniCompletion.completefunc_lsp')
+  client.resolved_capabilities.document_formatting = false
+end
+
+require'lspconfig'.r_language_server.setup({
+  on_attach = on_attach_custom,
+  -- Debounce "textDocument/didChange" notifications because they are slowly
+  -- processed (seen when going through completion list with `<C-N>`)
+  flags = { debounce_text_changes = 150 },
+})
+
+-- Python (pyright) ===========================================================
+require'lspconfig'.pyright.setup({ on_attach = on_attach_custom })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
